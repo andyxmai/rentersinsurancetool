0433a9921e18a60bc8e8c194af70f62e64ac8702
Andy Mai <andrew.x.mai@gmail.com>
2017-01-10 12:05:52 -0800

    remove unused component

--- src/quotes/components/QuotesForm.js
+++ src/quotes/components/QuotesForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './styles.css';
-import { Form, Col, FormControl, FormGroup, Button, ControlLabel, Checkbox, HelpBlock } from 'react-bootstrap';
+import { Form, Col, FormControl, FormGroup, Button, ControlLabel, HelpBlock } from 'react-bootstrap';
 
 function QuotesForm (props) {
   return (
