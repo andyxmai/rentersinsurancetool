fd3b6faf96b0d6db000c2f377d18b5abcb690e0d
Andy Mai <andrew.x.mai@gmail.com>
2017-01-09 19:34:05 -0800

    add 404 page

---
+++ src/NotFound/components/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+
+function NotFound (props) {
+  return (
+    <div className="container">
+      <h1>404 Not Found</h1>
+    </div>
+  )
+}
+
+export default NotFound;
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import MainContainer from './main/MainContainer'
 import LandingContainer from './landing/containers/Landing/LandingContainer';
 import QuotesContainer from './quotes/containers/QuotesContainer'
 import RI101Container from './rentersinsurance101/containers/RentersInsurance101Container';
+import NotFound from './NotFound/components/NotFound';
 
 const Routes = (props) => (
   <Router {...props}>
@@ -12,6 +13,7 @@ const Routes = (props) => (
       <IndexRoute component={LandingContainer} />
       <Route path="quotes" component={QuotesContainer} />
       <Route path="rentersinsurance101" component={RI101Container} />
+      <Route path="*" component={NotFound} />
     </Route>
   </Router>
 );
