38bc08322988637ca9b7283ef6846369ba0d9c61
Andy Mai <andrew.x.mai@gmail.com>
2017-01-14 17:11:35 -0800

    works for localhost:9000

--- server/app.js
+++ server/app.js
@@ -12,8 +12,8 @@ app.use(express.static(path.resolve(__dirname, '..', 'build')));
 
 // Always return the main index.html, so react-router render the route in the client
 app.get('/*', (req, res) => {
-//  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
-  res.sendFile(path.resolve(__dirname, '..', 'public', 'index.html'));
+  console.log(path.resolve(__dirname, '..', 'build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
 });
 
 module.exports = app;
